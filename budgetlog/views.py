f79cc6fcef8bd8811f1030e954e74e1c68be35d0
JaVoJ <davias@email.cz>
2024-11-24 15:07:29 +0100

    return to list page fixed
    Signed-off-by: JaVoJ <davias@email.cz>

--- budgetlog/views.py
+++ budgetlog/views.py
@@ -251,7 +251,7 @@ class BookUpdateView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['object_type'] = self.model._meta.verbose_name
-        context['list_url_name'] = f'{self.model._meta.model_name}-list'
+        context['list_url_name'] = self.success_url
         return context
 
 
